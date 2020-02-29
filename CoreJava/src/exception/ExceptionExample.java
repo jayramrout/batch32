c456544618e115d54f2a3b03e851f5bd3f6971e0
Jayram <jayram.rout@hotmail.com>
2020-02-28 21:01:42 -0500

    Updates

--- CoreJava/src/exception/ExceptionExample.java
+++ CoreJava/src/exception/ExceptionExample.java
@@ -8,6 +8,8 @@ public class ExceptionExample {
     public static void main(String[] args) {
         GeometricTeacher teacher = null;
 
+        System.out.println(teacher.getId());
+
         if(teacher != null){
             System.out.println(teacher.getId()); // from nothing you are trying to access something...
         }
